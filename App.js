a27cb7d1f40cb441845c3d25724b4f09d7ae81db
Bontog_R/github_username <ricianinbontog123@gmail.com>
2025-09-25 15:46:32 +0800

    Edited

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import MidAct01 from './MidAct01';
+
+import ColorChangerApp from './ColorChangerApp';
+import CounterApp from './CounterApp';
+import ChatBox from './ChatBox';
 import Comment from './Comment'
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
      
-     
-      <MidAct01/>
+      <ColorChangerApp/>
+      <CounterApp/>
+      <ChatBox/>
+      <Comment/>
     </SafeAreaView>
   );
 }
