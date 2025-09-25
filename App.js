ee0b6f9ad203a8f32fcdc97cba90cda65a9238de
Bontog_R/github_username <ricianinbontog123@gmail.com>
2025-09-25 17:05:04 +0800

    Update files

--- App.js
+++ App.js
@@ -3,16 +3,12 @@ import { SafeAreaView } from 'react-native';
 
 import ColorChangerApp from './ColorChangerApp';
 import CounterApp from './CounterApp';
-import ChatBox from './ChatBox';
-import Comment from './Comment'
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
      
       <ColorChangerApp/>
       <CounterApp/>
-      <ChatBox/>
-      <Comment/>
     </SafeAreaView>
   );
 }
